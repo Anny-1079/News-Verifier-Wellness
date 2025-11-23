5af94d496df9a812ac8dbaadec70391acdb12c59
Anny-1079 <165645231+Anny-1079@users.noreply.github.com>
2025-11-23 16:53:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,8 @@ def main():
     """
             try:
                 response = client.chat.completions.create(
-                    model="llama3-8b-8192",
+                    model="llama-3.1-8b-instant",
+                    # "llama3-8b-8192",
                     messages=[{"role": "user", "content": prompt}]
                 )
                 return response.choices[0].message.content.strip()
